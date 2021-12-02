64a0b4627e3f36b18a5c9457c5f0b9c546aced83
Matheus-Antunes03 <matheus_pol@hotmail.com>
2021-12-02 16:38:30 -0300

    Configuração de tela Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,51 @@
 import React from "react";
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
-        <h3>Página Dashboard</h3>
+        <>
+            <Title>Explore repositórios no GitHub</Title>
+            <Form>
+                <input placeholder = "Digite o nome do Repositório"/>
+                <button type = "submit">Pesquisar</button>
+            </Form>
+            <Repositories>
+                <Link to="#">
+                    <img src="https://pbs.twimg.com/media/Ehbm9LpWoAECUjx.jpg" alt="Araki"></img>
+                    <div>
+                        <strong>projetoReactMalwee</strong>
+                        <p>maos para o alto caraiokk</p>
+                        <button type="submit">Pesquisar</button>
+                    </div>
+                    <FiChevronRight/>
+                </Link>
+            </Repositories>
+            <Repositories>
+                <Link to="#">
+                    <img src="https://pbs.twimg.com/media/Ehbm9LpWoAECUjx.jpg" alt="Araki"></img>
+                    <div>
+                        <strong>projetoReactMalwee</strong>
+                        <p>maos para o alto caraiokk</p>
+                        <button type="submit">Pesquisar</button>
+                    </div>
+                    <FiChevronRight/>
+                </Link>
+            </Repositories>
+            <div>
+                <Link to="#">
+                    <img src="https://pbs.twimg.com/media/Ehbm9LpWoAECUjx.jpg" alt="Araki"></img>
+                    <div>
+                        <strong>projetoReactMalwee</strong>
+                        <p>maos para o alto caraiokk</p>
+                        <button type="submit">Pesquisar</button>
+                    </div>
+                    <FiChevronRight/>
+                </Link>
+            </div>
+        </>
     );
 }
 
