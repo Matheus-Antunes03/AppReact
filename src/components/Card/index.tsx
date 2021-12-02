54fe731115687235da15112ed5ee10a05cffe4a9
Matheus-Antunes03 <matheus_pol@hotmail.com>
2021-12-02 16:37:54 -0300

    Configuração de teste

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card: React.FC = () => {
+    return (
+        <>
+            <h3>Card</h3>
+            <p>vinicios lixo dmais slk kkkk</p>
+        </>
+    );
+}
+
+export default Card;
